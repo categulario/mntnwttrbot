8581a4e77a660cc1cae83e971d58e12a6771e823
Abraham Toriz Cruz <awonderfulcode@gmail.com>
2017-06-28 18:55:40 -0500

    add a new route

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,24 @@
 
 extern crate rocket;
 
+#[macro_use] extern crate rocket_contrib;
+
+use rocket_contrib::{JSON, Value};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> JSON<Value> {
+    return JSON(json!({ "ok": true}));
+}
+
+#[get("/<name>")]
+fn botname(name: &str) -> JSON<Value> {
+    return JSON(json!({
+        "name": name,
+    }));
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, botname])
+        .launch();
 }
